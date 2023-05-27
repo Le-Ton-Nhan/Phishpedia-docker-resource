1a9d5577fc8b59afeee513e1830a88b19eab7b6b
LamThanhNgan <lamthanhngan662607004@gmail.com>
2023-05-27 15:18:30 +0700

    :boom: vt scan code

--- phishpedia/src/util/chrome.py
+++ phishpedia/src/util/chrome.py
@@ -1,6 +1,6 @@
 import time
 import requests
-
+from collections import Counter
 
 def vt_scan(url_test):
     '''
@@ -9,12 +9,12 @@ def vt_scan(url_test):
     :return: # positive engines, # total engines
     '''
     retry = 0
-    api_key = "your own token"
+    api_key = "d665ea8a7c632253b0a18ede19d2d4bc1376e744710421c98d4d2b9df360cbe2"
     url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
     params = {'apikey': api_key, 'resource': url_test, 'scan':1}
     response = requests.get(url, params=params)
-    print("Response: ", response)
+    
     response = response.json()
 
     # This means the url wasnt in VT's database, preparing a new scan
@@ -29,9 +29,16 @@ def vt_scan(url_test):
     try:
         positive = response['positives']
         total = response['total']
+        scans_result = response['scans']
+        scans = []
+        for item in scans_result.values():
+            scans.append(item["result"])
+        counter = Counter(scans)
+        most_common = counter.most_common(1)[0][0]
     except KeyError:
         positive = None
         total = None
+        most_common = "Undefined"
 
-    return positive, total
+    return positive, total, most_common
 
